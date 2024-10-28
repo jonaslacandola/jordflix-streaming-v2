9a7b75e447c48e6c81988029ff90fe929025a251
jlaca <jonaslacandola07@gmail.com>
2024-10-28 11:00:23 +0800

    some changes

---
+++ src/app/home/_error.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <div className="w-full h-screen">
+      <p className="text-center text-2xl">Something went wrong</p>
+    </div>
+  );
+}
