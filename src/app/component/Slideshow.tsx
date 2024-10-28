5797a4e84b381237e372c7bd96871336bac3e794
jlaca <jonaslacandola07@gmail.com>
2024-10-28 10:38:49 +0800

    some changes

--- src/app/component/Slideshow.tsx
+++ src/app/component/Slideshow.tsx
@@ -7,27 +7,24 @@ import React, { useEffect, useRef, useState } from "react";
 export default function Slideshow({
   images,
 }: Readonly<{ images: Array<any> }>) {
-  const ref = useRef<Array<any>>(images);
   const [currentImage, setCurrentImage] = useState<number>(0);
 
   useEffect(() => {
     const imageInterval = setInterval(() => {
-      setCurrentImage((curr) =>
-        curr >= ref.current?.length - 1 ? 0 : curr + 1
-      );
+      setCurrentImage((curr) => (curr >= images?.length - 1 ? 0 : curr + 1));
     }, 5000);
 
     return () => clearInterval(imageInterval);
-  }, [ref.current?.length]);
+  }, [images?.length]);
 
   return (
     <div className="relative aspect-[10/8] md:aspect-[12/6] lg:aspect-[10/4] bg-slate-900 bg-opacity-50 transition-all duration-300 hover:bg-opacity-10">
       <div className="w-full h-full flex flex-col justify-center items-center gap-4 backdrop-blur-sm">
         <Link
-          href={`/movies/watch/${ref.current[currentImage].id}`}
+          href={`/movies/watch/${images[currentImage].id}`}
           className="text-sm lg:text-base"
         >
-          {ref.current[currentImage].title}
+          {images[currentImage].title}
         </Link>
         <h1 className="text-6xl font-black text-blue-600 xl:text-8xl">
           jordflix
@@ -39,7 +36,7 @@ export default function Slideshow({
       <Image
         alt="Jordflix Slideshow"
         fill
-        src={`https://image.tmdb.org/t/p/w500/${ref.current[currentImage].image}`}
+        src={`https://image.tmdb.org/t/p/w500/${images[currentImage].image}`}
         className="object-cover z-[-1]"
       />
     </div>
